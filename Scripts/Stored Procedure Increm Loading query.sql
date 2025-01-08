d187bb5631fcc961db9c1549c8e99f08f2446b40
Anmol Garg <anmolgarg848@gmail.com>
2025-01-08 14:38:45 +0530

    Add files via upload

---
+++ Scripts/Stored Procedure Increm Loading query.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE UpdateWatermarkTable
+    @lastload varchar(200)
+AS
+BEGIN
+ --Start the transaction
+  BEGIN TRANSACTION;
+
+--Update the incremental colmn in the table
+  UPDATE watermark_table 
+  SET last_load = @lastload
+
+ -- Commit transaction
+  COMMIT TRANSACTION;
+
+  END;
+
